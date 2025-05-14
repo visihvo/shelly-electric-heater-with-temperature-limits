d71415601eb164cb12d6f1624a7e2f90047c1984
visihvo <vilisihvo25gmail.com>
2025-05-14 14:51:27 +0300

    First version

---
+++ App.js
@@ -0,0 +1,90 @@
+// Check valid regions https://api.spot-hinta.fi/swagger/ui
+let PriceAllowed = "9"; // Set the maximum electricity price at which you want the device to be on (cents)
+let minTemp = 22.1; // Minimum temperature
+let maxTemp = 22.5; // Maximum temperature
+// =========================== SCRIPT, DO NOT EDIT LINES OR VALUES BELOW =============================
+//last known h,    hourly price fetched,   temperature rising,     relay closed,      price below limit
+let cHour = ""   ;   let fetched = false; let rising = false   ; let rclosed = true; let priceOk = false; 
+let urlToCall = "https://api.spot-hinta.fi/JustNowRank/0/" + PriceAllowed;
+
+/**
+ * If-statements and their assignments prevent future needs to fetch price status again
+ * @param {JSON} res HTTP GET results of fetch 
+ * @returns price ok -> true, price too expensive -> false, invalid response -> doesnt assign
+ * global variables fetch resulting in being called again later
+ */
+function handleResponse(res) {
+  // If status code is 200, the price is OK
+  if (res.code === 200) { priceOk = true; fetched = true; return; }
+  // If status code is 400, the price is too high
+  if (res.code === 400) { priceOk = false; fetched = true; return; }
+  // If unexpected response, assume price is not OK
+  priceOk = false;
+}
+
+/**
+ * Temperature fetch and its error handling.
+ * @returns succesful fetch -> current temperature as a number, 
+ * else returns "error"
+ */
+function getTemp() {
+  let temp;
+  try {
+      temp = Shelly.getComponentStatus('Temperature', 100).tC;  //Temp ID, mostly 100 to 102 <- default values
+      print(temp);
+  } catch(error) { print(error); temp = "error"; }
+  return temp;
+}
+
+/**
+ * Actual relay function and logic related to it.
+ * @param {string} status "stop" -> relay switches off, anything else -> relay switches on
+ */
+function manageWarming(status) {
+  if (status === "stop") {
+    Shelly.call("Switch.Set", "{ id:0, on:false}", null, null); 
+    print("Relay OFF"); 
+    rising = false; 
+    rclosed = true;
+  }
+  else {
+    Shelly.call("Switch.Set", "{ id:0, on:true}", null, null); 
+    print("Relay ON"); 
+    rising = true;
+    rclosed = false;
+  }
+}
+
+/**
+ * Responsible for handling proper calls for relay work.
+ */
+function handleWarming() {
+  // cant fetch price or its too high and relay is on results in it closing
+  if (!priceOk && !rclosed) { manageWarming("stop"); }
+  //price too high or invalid fetch but relay is already closed
+  else if (!priceOk) { return; } 
+  
+  let temp = getTemp();
+  
+  // invalid fetch for temperature
+  if (typeof temp !== "number") { manageWarming("stop"); } 
+  
+  // temperature has risen below the set limit  
+  if (temp > maxTemp && rising) { manageWarming("stop"); }
+
+  //temperate has gone below the set limit
+  else if (minTemp > temp && !rising) { rising = true; manageWarming("start"); }
+}
+
+/**
+ * "Main function"
+ */
+Timer.set(30000, true, function () {
+  Shelly.call("Shelly.GetStatus", "", function (res) {
+    let hour = res.sys.time.slice(0, 2); // f.ex. "21:34"
+    if (cHour !== hour) { cHour = hour; fetched = false; }
+    if (fetched === true) { print("The price for this hour is already fetched."); return; }
+    Shelly.call("HTTP.GET", { url: urlToCall, timeout: 15, ssl_ca: "*" }, handleResponse);
+  });
+  handleWarming();  
+});
